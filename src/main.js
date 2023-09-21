775b7c2179ee23c6bbe1a266b36acbca88c3f9d5
Lucag502 <84347577+Lucag502@users.noreply.github.com>
2023-09-21 20:57:03 -0300

    agragando explorePreview y desarrollando slider

--- src/main.js
+++ src/main.js
@@ -24,10 +24,38 @@ async function getTrendingMovies(){
         
 
     });
+    
 
     
 }
 
+async function getExploreMovies(){
+    const res = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=' + API_KEY);
+    const data = await res.json();
 
+    const movies = data.results;
+    movies.forEach(movie => {
+        const exploreMovies = document.querySelector('#explorePreviewMovies')
+        const movieContainer = document.createElement('div');
+        const slider = document.querySelector('#slider');
+        movieContainer.classList.add('movie-container');
 
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+
+        movieImg.setAttribute('src',
+        'https://image.tmdb.org/t/p/w300' + movie.poster_path,);
+
+        
+        movieContainer.appendChild(movieImg);
+        slider.appendChild(movieContainer);
+        exploreMovies.appendChild(slider);
+    });
+    
+
+    
+}
+
+getExploreMovies();
 getTrendingMovies();
