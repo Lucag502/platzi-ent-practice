f56cdfed1becdefcf75961a6d5ec439d1e1deec8
Lucag502 <84347577+Lucag502@users.noreply.github.com>
2023-09-20 21:06:20 -0300

    añadiendo slider y las imagenes de la API

--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
-// async function getTrendingMovies
+async function getTrendingMovies(){
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await res.json();
+
+    const movies = data.results;
+    movies.forEach(movie => {
+        const trendingMovies = document.querySelector('#trendingPreviewMovies')
+        const movieContainer = document.createElement('div');
+        const slider = document.querySelector('#slider');
+        movieContainer.classList.add('movie-container');
+
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+
+        movieImg.setAttribute('src',
+        'https://image.tmdb.org/t/p/w300' + movie.poster_path,);
+
+        
+        movieContainer.appendChild(movieImg);
+        slider.appendChild(movieContainer);
+        trendingMovies.appendChild(slider);
+        
+        
+
+    });
+
+    
+}
+
+
+
+getTrendingMovies();
